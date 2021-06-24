34fa9ab37a0ab1a8d1be74fe4c860080e448ec51
Cheng Pak Yim <u3578494@connect.hku.hk>
2021-06-24 15:42:40 +0800

    Code tidying
    Put the facial landmark stuff in a seperate class to modulize them

--- facial_landmark.py
+++ facial_landmark.py
@@ -1,67 +1,79 @@
 """
-Main program to run the detection
+For finding the face and face landmarks for further manipulication
 """
 
 import cv2
 import mediapipe as mp
 import numpy as np
 
-# for TCP connection with unity
-import socket
-from collections import deque
-from platform import system
+class FaceMeshDetector:
+    def __init__(self,
+                 static_image_mode=False,
+                 max_num_faces=1,
+                 min_detection_confidence=0.5,
+                 min_tracking_confidence=0.5):
 
-from pose_estimator import PoseEstimator
-from stabilizer import Stabilizer
+        self.static_image_mode = static_image_mode
+        self.max_num_faces = max_num_faces
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
 
-connect = True
-port = 5066         # have to be same as unity
+        # Facemesh
+        self.mp_face_mesh = mp.solutions.face_mesh
+        # The object to do the stuffs
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
+            self.static_image_mode,
+            self.max_num_faces,
+            self.min_detection_confidence,
+            self.min_tracking_confidence
+        )
 
-# init TCP connection with unity
-# return the socket connected
-def init_TCP():
-    address = ('127.0.0.1', port)
-    # address = ('192.168.0.107', port)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # print(socket.gethostbyname(socket.gethostname()))
-    s.connect(address)
-    return s
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-def send_info_to_unity(s, args):
-    msg = '%.4f %.4f %.4f' % args
-    s.send(bytes(msg, "utf-8"))
+    def findFaceMesh(self, img, draw=True):
+        # convert the img from BRG to RGB
+        img = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        img.flags.writeable = False
+        self.results = self.face_mesh.process(img)
+
+        # Draw the face mesh annotations on the image.
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        self.imgH, self.imgW, self.imgC = img.shape
+
+        self.faces = []
+
+        if self.results.multi_face_landmarks:
+            for face_landmarks in self.results.multi_face_landmarks:
+                if draw:
+                    self.mp_drawing.draw_landmarks(
+                        image = img,
+                        landmark_list = face_landmarks,
+                        connections = self.mp_face_mesh.FACE_CONNECTIONS,
+                        landmark_drawing_spec = self.drawing_spec,
+                        connection_drawing_spec = self.drawing_spec)
+
+                face = []
+                for id, lmk in enumerate(face_landmarks.landmark):
+                    x, y = int(lmk.x * self.imgW), int(lmk.y * self.imgH)
+                    face.append([x, y])
+
+                self.faces.append(face)
+
+        return img, self.faces
 
 
+# sample run of the module 
 def main():
 
+    detector = FaceMeshDetector()
+
     cap = cv2.VideoCapture(0)
-
-    # Facemesh
-    mp_face_mesh = mp.solutions.face_mesh
-    # The object to do the stuffs
-    face_mesh = mp_face_mesh.FaceMesh()
-
-    mp_drawing = mp.solutions.drawing_utils
-    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-
-    # get a sample frame for pose estimation img
-    success, img = cap.read()
-
-    # Pose estimation related
-    pose_estimator = PoseEstimator((img.shape[0], img.shape[1]))
-    image_points = np.zeros((pose_estimator.model_points_full.shape[0], 2))
-
-    # Introduce scalar stabilizers for pose.
-    pose_stabilizers = [Stabilizer(
-        state_num=2,
-        measure_num=1,
-        cov_process=0.1,
-        cov_measure=0.1) for _ in range(6)]
-
-
-    # Initialize TCP connection
-    if connect:
-        socket = init_TCP()
 
     while cap.isOpened():
         success, img = cap.read()
@@ -70,94 +82,11 @@ def main():
             print("Ignoring empty camera frame.")
             continue
 
-        # convert the img from BRG to RGB
-        img = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+        img, faces = detector.findFaceMesh(img)
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        img.flags.writeable = False
-        results = face_mesh.process(img)
+        if faces:
+            print(faces[0])
 
-        # Draw the face mesh annotations on the image.
-        img.flags.writeable = True
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-        imgH, imgW, imgC = img.shape
-
-        # Pose estimation by 3 steps:
-        # 1. detect face;
-        # 2. detect landmarks;
-        # 3. estimate pose
-
-        # Here is the first two steps
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image = img,
-                    landmark_list = face_landmarks,
-                    connections = mp_face_mesh.FACE_CONNECTIONS,
-                    landmark_drawing_spec = drawing_spec,
-                    connection_drawing_spec = drawing_spec)
-
-                # print(face_landmarks)
-
-                for id, lmk in enumerate(face_landmarks.landmark):
-                    x, y = int(lmk.x * imgW), int(lmk.y * imgH)
-                    image_points[id, 0] = x
-                    image_points[id, 1] = y
-
-            # The third step: pose estimation
-            # pose: [[rvec], [tvec]]
-            pose = pose_estimator.solve_pose_by_all_points(image_points)
-
-            # print("rvec (y) = (%f): " % (pose[0][1]))
-            # print("rvec (x, y, z) = (%f, %f, %f): " % (pose[0][0], pose[0][1], pose[0][2]))
-            # print("tvec (x, y, z) = (%f, %f, %f): " % (pose[1][0], pose[1][1], pose[1][2]))
-
-            # Stabilize the pose.
-            steady_pose = []
-            pose_np = np.array(pose).flatten()
-
-            for value, ps_stb in zip(pose_np, pose_stabilizers):
-                ps_stb.update([value])
-                steady_pose.append(ps_stb.state[0])
-
-            steady_pose = np.reshape(steady_pose, (-1, 3))
-
-            # print("rvec (x, y, z) = (%f, %f, %f): " % (steady_pose[0][0], steady_pose[0][1], steady_pose[0][2]))
-            # print("tvec steady (x, y, z) = (%f, %f, %f): " % (steady_pose[1][0], steady_pose[1][1], steady_pose[1][2]))
-
-
-            # calculate the roll/ pitch/ yaw
-            # roll: +ve when the axis pointing upward
-            # pitch: +ve when we look upward
-            # yaw: +ve when we look left
-            roll = np.clip(np.degrees(steady_pose[0][1]), -90, 90)
-            pitch = np.clip(-(180 + np.degrees(steady_pose[0][0])), -90, 90)
-            yaw =  np.clip(np.degrees(steady_pose[0][2]), -90, 90)
-
-            print("Roll: %.2f, Pitch: %.2f, Yaw: %.2f" % (roll, pitch, yaw))
-
-            # send info to unity
-            if connect:
-                send_info_to_unity(socket, (roll, pitch, yaw))
-
-
-            # pose_estimator.draw_annotation_box(img, pose[0], pose[1], color=(255, 128, 128))
-
-            # pose_estimator.draw_axis(img, pose[0], pose[1])
-
-            pose_estimator.draw_axes(img, steady_pose[0], steady_pose[1])
-
-        else:
-            # reset our pose estimator
-            pose_estimator = PoseEstimator((img.shape[0], img.shape[1]))
-
-
-
-
-        # flip vertically at the end for creating mirror img
-        # img = cv2.flip(img, 1)
         cv2.imshow('MediaPipe FaceMesh', img)
 
         # press "q" to leave
---
+++ main.py
@@ -0,0 +1,147 @@
+"""
+Main program to run the detection
+"""
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+# for TCP connection with unity
+import socket
+from collections import deque
+from platform import system
+
+# face detection and facial landmark
+from facial_landmark import FaceMeshDetector
+
+# pose estimation and stablization
+from pose_estimator import PoseEstimator
+from stabilizer import Stabilizer
+
+# global variable
+connect = False
+port = 5066         # have to be same as unity
+
+# init TCP connection with unity
+# return the socket connected
+def init_TCP():
+    address = ('127.0.0.1', port)
+    # address = ('192.168.0.107', port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # print(socket.gethostbyname(socket.gethostname()))
+    s.connect(address)
+    return s
+
+def send_info_to_unity(s, args):
+    msg = '%.4f %.4f %.4f' % args
+    s.send(bytes(msg, "utf-8"))
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+
+    # Facemesh
+    detector = FaceMeshDetector()
+
+    # get a sample frame for pose estimation img
+    success, img = cap.read()
+
+    # Pose estimation related
+    pose_estimator = PoseEstimator((img.shape[0], img.shape[1]))
+    image_points = np.zeros((pose_estimator.model_points_full.shape[0], 2))
+
+    # Introduce scalar stabilizers for pose.
+    pose_stabilizers = [Stabilizer(
+        state_num=2,
+        measure_num=1,
+        cov_process=0.1,
+        cov_measure=0.1) for _ in range(6)]
+
+
+    # Initialize TCP connection
+    if connect:
+        socket = init_TCP()
+
+    while cap.isOpened():
+        success, img = cap.read()
+
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        # Pose estimation by 3 steps:
+        # 1. detect face;
+        # 2. detect landmarks;
+        # 3. estimate pose
+
+        # first two steps
+        img, faces = detector.findFaceMesh(img)
+
+        # if there is any face detected
+        if faces:
+            # only get the first face
+            for i in range(len(image_points)):
+                image_points[i, 0] = faces[0][i][0]
+                image_points[i, 1] = faces[0][i][1]
+
+            # The third step: pose estimation
+            # pose: [[rvec], [tvec]]
+            pose = pose_estimator.solve_pose_by_all_points(image_points)
+
+            # print("rvec (y) = (%f): " % (pose[0][1]))
+            # print("rvec (x, y, z) = (%f, %f, %f): " % (pose[0][0], pose[0][1], pose[0][2]))
+            # print("tvec (x, y, z) = (%f, %f, %f): " % (pose[1][0], pose[1][1], pose[1][2]))
+
+            # Stabilize the pose.
+            steady_pose = []
+            pose_np = np.array(pose).flatten()
+
+            for value, ps_stb in zip(pose_np, pose_stabilizers):
+                ps_stb.update([value])
+                steady_pose.append(ps_stb.state[0])
+
+            steady_pose = np.reshape(steady_pose, (-1, 3))
+
+            # print("rvec (x, y, z) = (%f, %f, %f): " % (steady_pose[0][0], steady_pose[0][1], steady_pose[0][2]))
+            # print("tvec steady (x, y, z) = (%f, %f, %f): " % (steady_pose[1][0], steady_pose[1][1], steady_pose[1][2]))
+
+            # calculate the roll/ pitch/ yaw
+            # roll: +ve when the axis pointing upward
+            # pitch: +ve when we look upward
+            # yaw: +ve when we look left
+            roll = np.clip(np.degrees(steady_pose[0][1]), -90, 90)
+            pitch = np.clip(-(180 + np.degrees(steady_pose[0][0])), -90, 90)
+            yaw =  np.clip(np.degrees(steady_pose[0][2]), -90, 90)
+
+            print("Roll: %.2f, Pitch: %.2f, Yaw: %.2f" % (roll, pitch, yaw))
+
+            # send info to unity
+            if connect:
+                send_info_to_unity(socket, (roll, pitch, yaw))
+
+
+            # pose_estimator.draw_annotation_box(img, pose[0], pose[1], color=(255, 128, 128))
+
+            # pose_estimator.draw_axis(img, pose[0], pose[1])
+
+            pose_estimator.draw_axes(img, steady_pose[0], steady_pose[1])
+
+        else:
+            # reset our pose estimator
+            pose_estimator = PoseEstimator((img.shape[0], img.shape[1]))
+
+
+        # flip vertically at the end for creating mirror img
+        # img = cv2.flip(img, 1)
+        cv2.imshow('Facial landmark', img)
+
+        # press "q" to leave
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+
+
+if __name__ == "__main__":
+    # demo code
+    main()
